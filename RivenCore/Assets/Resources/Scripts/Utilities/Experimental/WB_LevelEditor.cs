bfdfe7285f32e031daee09b00d4b85d1b5145a9b
lizband <lizband@kbm5100>
2024-01-22 19:47:54 -0500

    Added level editor paint and tool modes

--- RivenCore/Assets/Resources/Scripts/Utilities/Experimental/WB_LevelEditor.cs
+++ RivenCore/Assets/Resources/Scripts/Utilities/Experimental/WB_LevelEditor.cs
@@ -31,6 +31,8 @@ public class WB_LevelEditor : MonoBehaviour
     private Entity editorPlayer;
     private Vector3 cursorPos;
     private int currentDepth;
+    private string currentTool = "paint";
+    private int currentPaintMode = 3;
 
 
     //=-----------------=
@@ -39,6 +41,8 @@ public class WB_LevelEditor : MonoBehaviour
     private System_LevelManager levelManager;
     private Camera viewCamera;
     [SerializeField] private Button[] topbarButtons;
+    [SerializeField] private Button Bttn_Inspect, Bttn_Paint;
+    [SerializeField] private Sprite[] paintModeSprites;
     [SerializeField] private Button[] sidebarButtons;
     [SerializeField] private Button[] hotbarButtons;
     [SerializeField] private GameObject inventory;
@@ -63,6 +67,7 @@ public class WB_LevelEditor : MonoBehaviour
 
         UserInput();
         UpdateHotbarImages();
+        UpdateToolImages();
     }
 
     //=-----------------=
@@ -76,24 +81,33 @@ public class WB_LevelEditor : MonoBehaviour
         topbarButtons[2].onClick.AddListener(() => { levelManager.LevelFile("Save"); });
         topbarButtons[3].onClick.AddListener(() => { StartTest(); });
         topbarButtons[4].onClick.AddListener(() => { StopTest(); });
-        hotbarButtons[0].onClick.AddListener(() => { currentHotbarIndex = 0; });
-        hotbarButtons[1].onClick.AddListener(() => { currentHotbarIndex = 1; });
-        hotbarButtons[2].onClick.AddListener(() => { currentHotbarIndex = 2; });
-        hotbarButtons[3].onClick.AddListener(() => { currentHotbarIndex = 3; });
-        hotbarButtons[4].onClick.AddListener(() => { currentHotbarIndex = 4; });
-        hotbarButtons[5].onClick.AddListener(() => { currentHotbarIndex = 5; });
-        hotbarButtons[6].onClick.AddListener(() => { currentHotbarIndex = 6; });
-        hotbarButtons[7].onClick.AddListener(() => { currentHotbarIndex = 7; });
-        hotbarButtons[8].onClick.AddListener(() => { currentHotbarIndex = 8; });
-        hotbarButtons[9].onClick.AddListener(() => { currentHotbarIndex = 9; });
+        Bttn_Inspect.onClick.AddListener(() => { currentTool = "inspect"; });
+        Bttn_Paint.onClick.AddListener(() => { currentTool = "paint"; });
+        hotbarButtons[0].onClick.AddListener(() => { currentHotbarIndex = 0; CheckPaintMode(); });
+        hotbarButtons[1].onClick.AddListener(() => { currentHotbarIndex = 1; CheckPaintMode(); });
+        hotbarButtons[2].onClick.AddListener(() => { currentHotbarIndex = 2; CheckPaintMode(); });
+        hotbarButtons[3].onClick.AddListener(() => { currentHotbarIndex = 3; CheckPaintMode(); });
+        hotbarButtons[4].onClick.AddListener(() => { currentHotbarIndex = 4; CheckPaintMode(); });
+        hotbarButtons[5].onClick.AddListener(() => { currentHotbarIndex = 5; CheckPaintMode(); });
+        hotbarButtons[6].onClick.AddListener(() => { currentHotbarIndex = 6; CheckPaintMode(); });
+        hotbarButtons[7].onClick.AddListener(() => { currentHotbarIndex = 7; CheckPaintMode(); });
+        hotbarButtons[8].onClick.AddListener(() => { currentHotbarIndex = 8; CheckPaintMode(); });
+        hotbarButtons[9].onClick.AddListener(() => { currentHotbarIndex = 9; CheckPaintMode(); });
         hotbarButtons[10].onClick.AddListener(() => { ToggleInventoryOpen(); });
     }
     
     private void UserInput()
     {
         // Place/Erase
-        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) Place();
-        if (Input.GetMouseButton(1) && !EventSystem.current.IsPointerOverGameObject()) { /*EraseTile(pos);*/ }
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            if (currentTool=="paint") Place();
+        }
+
+        if (Input.GetMouseButton(1) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            if (currentTool=="paint") Erase();
+        }
         
         // Save/Open
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.S)) levelManager.LevelFile("Save");
@@ -104,54 +118,86 @@ public class WB_LevelEditor : MonoBehaviour
         else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Z)) Undo();
         
         // Tab toggle inventory
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            //InitializeTileInventory();
-            ToggleInventoryOpen();
-        }
+        if (Input.GetKeyDown(KeyCode.Tab)) ToggleInventoryOpen();
         
         // Scroll wheel hotbar
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             currentHotbarIndex = (currentHotbarIndex + hotbarTileID.Length - 1) % hotbarTileID.Length;
+            CheckPaintMode();
         }
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             currentHotbarIndex = (currentHotbarIndex + hotbarTileID.Length - -1) % hotbarTileID.Length;
+            CheckPaintMode();
         }
     }
     
     private void Place()
     {
-        // Are we placing a tile?
-        if (levelManager.GetTileFromMemory(hotbarTileID[currentHotbarIndex]))
+        switch (currentPaintMode)
         {
-            TileBase tile = levelManager.GetTileFromMemory(hotbarTileID[currentHotbarIndex]);
-            Vector3Int position = new Vector3Int((int)MathF.Floor(cursorPos.x), (int)MathF.Floor(cursorPos.y), 0);
-            currentTilemap.SetTile(position, tile);
-        }
-
-        // Are we placing an asset?
-        else if (levelManager.GetAssetFromMemory(hotbarTileID[currentHotbarIndex]))
-        {
-            
-            // Destroy any asset already in the selected position
-            for (int i = 0; i < levelManager.assetsRoot.transform.childCount; i++)
+            // Are we placing a tile?
+            case 0:
+            case 2:
             {
-                if (levelManager.assetsRoot.transform.GetChild(i).transform.position == new Vector3(MathF.Round(cursorPos.x),
-                        MathF.Round(cursorPos.y), levelManager.assetsRoot.transform.GetChild(i).gameObject.transform.position.z+currentDepth))
-                {
-                    Destroy(levelManager.assetsRoot.transform.GetChild(i).gameObject);
-                }
+                TileBase tile = levelManager.GetTileFromMemory(hotbarTileID[currentHotbarIndex]);
+                Vector3Int position = new Vector3Int((int)MathF.Floor(cursorPos.x), (int)MathF.Floor(cursorPos.y), 0);
+                currentTilemap.SetTile(position, tile);
+                break;
             }
+            // Are we placing an asset?
+            case 1:
+            {
+                // Destroy any asset already in the selected position
+                for (int i = 0; i < levelManager.assetsRoot.transform.childCount; i++)
+                {
+                    if (levelManager.assetsRoot.transform.GetChild(i).transform.position == new Vector3(MathF.Round(cursorPos.x),
+                            MathF.Round(cursorPos.y), levelManager.assetsRoot.transform.GetChild(i).gameObject.transform.position.z+currentDepth))
+                    {
+                        Destroy(levelManager.assetsRoot.transform.GetChild(i).gameObject);
+                    }
+                }
 
-            // Place the current asset at the selected position
-            var asset = levelManager.GetAssetFromMemory(hotbarTileID[currentHotbarIndex]);
-            // We are using the current depth + the asset prefabs depth so that certain assets (like lights and entities) can use the z position as an offset
-            var assetRef = Instantiate(asset,
-                new Vector3(MathF.Round(cursorPos.x), MathF.Round(cursorPos.y), asset.transform.position.z+currentDepth), 
-                new Quaternion(0, 0, 0, 0), levelManager.assetsRoot.transform);
-            assetRef.name = assetRef.name.Replace("(Clone)","").Trim();
+                // Place the current asset at the selected position
+                var asset = levelManager.GetAssetFromMemory(hotbarTileID[currentHotbarIndex]);
+                // We are using the current depth + the asset prefabs depth so that certain assets (like lights and entities) can use the z position as an offset
+                var assetRef = Instantiate(asset,
+                    new Vector3(MathF.Round(cursorPos.x), MathF.Round(cursorPos.y), asset.transform.position.z+currentDepth), 
+                    new Quaternion(0, 0, 0, 0), levelManager.assetsRoot.transform);
+                assetRef.name = assetRef.name.Replace("(Clone)","").Trim();
+                break;
+            }
+        }
+    }
+    
+    private void Erase()
+    {
+        switch (currentPaintMode)
+        {
+            // Are we placing a tile?
+            case 0:
+            case 2:
+            {
+                Vector3Int position = new Vector3Int((int)MathF.Floor(cursorPos.x), (int)MathF.Floor(cursorPos.y), 0);
+                currentTilemap.SetTile(position, null);
+                break;
+            }
+            // Are we placing an asset?
+            case 1:
+            {
+                // Destroy any asset already in the selected position
+                for (int i = 0; i < levelManager.assetsRoot.transform.childCount; i++)
+                {
+                    if (levelManager.assetsRoot.transform.GetChild(i).transform.position == new Vector3(MathF.Round(cursorPos.x),
+                            MathF.Round(cursorPos.y), levelManager.assetsRoot.transform.GetChild(i).gameObject.transform.position.z+currentDepth))
+                    {
+                        Destroy(levelManager.assetsRoot.transform.GetChild(i).gameObject);
+                    }
+                }
+
+                break;
+            }
         }
     }
     
@@ -161,6 +207,14 @@ public class WB_LevelEditor : MonoBehaviour
 
     private void Redo()
     {
+    }
+
+    private void CheckPaintMode()
+    {
+        if (levelManager.GetTileFromMemory(hotbarTileID[currentHotbarIndex]) && !hotbarTileID[currentHotbarIndex].Contains("Collision")) currentPaintMode = 0;
+        else if (levelManager.GetAssetFromMemory(hotbarTileID[currentHotbarIndex])) currentPaintMode = 1;
+        else if (levelManager.GetTileFromMemory(hotbarTileID[currentHotbarIndex]) && hotbarTileID[currentHotbarIndex].Contains("Collision")) currentPaintMode = 2;
+        else currentPaintMode = 3; // This is the ID for a missing/null paint mode
     }
 
     private void UpdateHotbarImages()
@@ -186,6 +240,24 @@ public class WB_LevelEditor : MonoBehaviour
             if (i != currentHotbarIndex) hotbarSelection.SetActive(false);
             else hotbarSelection.SetActive(true);
         }
+    }
+
+    private void UpdateToolImages()
+    {
+        var paintImage = Bttn_Paint.transform.GetChild(0).GetComponent<Image>();
+        var inspectImage = Bttn_Inspect.transform.GetChild(0).GetComponent<Image>();
+        switch (currentTool)
+        {
+            case "paint":
+                paintImage.color = new Color(1, 1, 1, 1);
+                inspectImage.color = new Color(1, 1, 1, 0.25f);
+                break;
+            case "inspect":
+                paintImage.color = new Color(1, 1, 1, 0.25f);
+                inspectImage.color = new Color(1, 1, 1, 1);
+                break;
+        }
+        paintImage.sprite = paintModeSprites[currentPaintMode];
     }
     
     private void ToggleInventoryOpen()
