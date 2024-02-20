74052cd32722b2111b5535267e08e4357488907c
lizband <lizband@kbm5100>
2024-02-20 16:10:40 -0500

    Added autumn ruins push box, button, switch, and pillar

--- RivenCore/Assets/Resources/Scripts/Integratables/Trigger_Event.cs
+++ RivenCore/Assets/Resources/Scripts/Integratables/Trigger_Event.cs
@@ -35,6 +35,7 @@ public class Trigger_Event : MonoBehaviour
     //=-----------------=
     private Logic_Processor logicProcessor;
     public List<Entity> entitiesInTrigger = new List<Entity>();
+    public List<Rigidbody2D> propsInTrigger = new List<Rigidbody2D>();
 
 
     //=-----------------=
@@ -47,13 +48,16 @@ public class Trigger_Event : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D _other)
     {
         // Exit if not a target, or activated & one time use
-        if (!_other.CompareTag("Entity") || wasActivated && !resetsAfterUse) return;
-        // get the target entity
+        if (!_other.CompareTag("Entity") && !_other.CompareTag("PhysProp") || wasActivated && !resetsAfterUse) return;
+        // get the target entity/prop
         var targetEnt = _other.gameObject.transform.parent.GetComponent<Entity>();
-        // Exit if not able to get entity component
-        if (!targetEnt) return;
-        // Add the entity to the list if it's not already
-        if (!entitiesInTrigger.Contains(targetEnt)) entitiesInTrigger.Add(targetEnt);
+        var targetProp = _other.gameObject.transform.parent.GetComponent<Rigidbody2D>();
+        if (targetProp) print("Target Prop found");
+        // Exit if not able to get entity/prop component
+        if (!targetEnt && !targetProp) return;
+        // Add the entity/prop to the list if it's not already
+        if (targetEnt) if (!entitiesInTrigger.Contains(targetEnt)) entitiesInTrigger.Add(targetEnt);
+        if (targetProp) if (!propsInTrigger.Contains(targetProp)) propsInTrigger.Add(targetProp);
         
         // Update the trigger state
         Interact();
@@ -62,13 +66,15 @@ public class Trigger_Event : MonoBehaviour
     private void OnTriggerExit2D(Collider2D _other)
     {
         // Exit if not a target, or activated & one time use
-        if (!_other.CompareTag("Entity") || wasActivated && !resetsAfterUse) return;
+        if (!_other.CompareTag("Entity") && !_other.CompareTag("PhysProp") || wasActivated && !resetsAfterUse) return;
         // get the target entity
         var targetEnt = _other.gameObject.transform.parent.GetComponent<Entity>();
-        // Exit if not able to get entity component
-        if (!targetEnt) return;
+        var targetProp = _other.gameObject.transform.parent.GetComponent<Rigidbody2D>();
+        // Exit if not able to get entity/prop component
+        if (!targetEnt && !targetProp) return;
         // Remove the entity from the list if it's not already
-        if (entitiesInTrigger.Contains(targetEnt)) entitiesInTrigger.Remove(targetEnt);
+        if (targetEnt) if (entitiesInTrigger.Contains(targetEnt)) entitiesInTrigger.Remove(targetEnt);
+        if (targetProp) if (propsInTrigger.Contains(targetProp)) propsInTrigger.Remove(targetProp);
         
         // Update the trigger state
         Interact();
@@ -87,13 +93,13 @@ public class Trigger_Event : MonoBehaviour
         
         print(entitiesInTrigger.Count);
         // Is an entity in the trigger?
-        if (entitiesInTrigger.Count > 0)
+        if (entitiesInTrigger.Count > 0 || propsInTrigger.Count > 0)
         {
             if (onOccupiedSignal != "") logicProcessor.UpdateState(onOccupiedSignal, true);
             if (onUnoccupiedSignal != "") logicProcessor.UpdateState(onUnoccupiedSignal, false);
         }
         // Have all entities left the trigger?
-        if (entitiesInTrigger.Count == 0)
+        if (entitiesInTrigger.Count == 0 && propsInTrigger.Count == 0)
         {
             if (onOccupiedSignal != "") logicProcessor.UpdateState(onOccupiedSignal, false);
             if (onUnoccupiedSignal != "") logicProcessor.UpdateState(onUnoccupiedSignal, true);
